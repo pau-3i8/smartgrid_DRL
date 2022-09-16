0e7979522a7a55df2a55731597472ff6f658c5af
pau-3i8 <69418753+pau-3i8@users.noreply.github.com>
2022-09-16 23:09:37 +0200

    Update env_parallelization.py

--- intrinsic_param_study/env_parallelization.py
+++ intrinsic_param_study/env_parallelization.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     # optuna handles the parallelization through their distributed implementation.
     for worker in range(N_WORKERS):
         psutil.Process().cpu_affinity({worker}) #allows fixing a processor number and use it as a seed. All distributed trials will have different seeds and allow reproducibility
-        subprocess.Popen("python3 env_hyperparam_tuning.py", shell=True)
+        subprocess.Popen("python3 env_tuning.py", shell=True)
         # add a wait time to avoid race conditions
         time.sleep(2)
